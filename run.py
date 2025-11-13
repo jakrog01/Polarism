6aa91dac9bfaee3cedd005c873931784b2ff0010
jakub <rogala.jk@gmail.com>
2025-11-13 12:56:29 +0100

    Add reservoir

--- run.py
+++ run.py
@@ -7,6 +7,7 @@ def main():
     boundry_condition_strategy = ps.BoundaryCondition(grid, cfg.boundry_condition, cfg.physics)
     potential = ps.create_potential(cfg.potential, grid)
     lasers = [ps.LaserFactory.create_laser(cfg.laser) for _ in range(cfg.laser.laser_count)]
+    reservoir = ps.create_reservoir(cfg.reservoir, grid)
 
 if __name__ == "__main__":
     main()
