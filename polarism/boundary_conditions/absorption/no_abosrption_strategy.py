85616cc2cf8e6862eb6f171549a3878335bfb4fc
jakub <rogala.jk@gmail.com>
2025-11-14 17:35:30 +0100

    Add NoAbsorption strategy

--- polarism/boundary_conditions/absorption/no_abosrption_strategy.py
+++ polarism/boundary_conditions/absorption/no_abosrption_strategy.py
@@ -1,16 +1,7 @@
-from polarism.boundary_conditions.absorption.absorption_strategy import AbsorptionStrategy
-from polarism.boundary_conditions.absorption.absorption_registry import register_absorption
-<<<<<<< HEAD
-
-@register_absorption("no-absorption")
-class NoAbsorptionStrategy(AbsorptionStrategy):
-    def before_step_action(self):
-        return 0.0
-
-    def after_step_action(self, psi):
-=======
 from polarism.simulation_grid_2D import SimulationGrid2D
 from polarism.config.simulation_parameters import BoundaryConditionParameters, PhysicsConstants
+from polarism.boundary_conditions.absorption.absorption_registry import register_absorption
+from polarism.boundary_conditions.absorption.absorption_strategy import AbsorptionStrategy
 
 @register_absorption("no-absorption")
 class NoAbsorptionStrategy(AbsorptionStrategy):
@@ -21,5 +12,4 @@ class NoAbsorptionStrategy(AbsorptionStrategy):
         return 0.0
 
     def apply_absorption(self, psi):
->>>>>>> b73a8ee (Add NoAbsorption strategy)
         return psi
